c167d98fd03675c8ff342e9293dcb8f7b40747c7
ashrafbd1496 <asraf1496@gmail.com>
2024-01-05 09:58:07 +0600

    vue wp starter

---
+++ src/admin/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      msg: "Welcome vue wordpress plugin development"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
